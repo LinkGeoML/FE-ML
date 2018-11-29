a5d5fb38f817decf8ebe725f69295df8123f2b07
MithrandirGr <nikos.kostagiolas@gmail.com>
2018-11-29 15:38:05 +0200

    Completed migration to geopandas

--- geospatial-feature-extraction/pois_feature_extraction.py
+++ geospatial-feature-extraction/pois_feature_extraction.py
@@ -1,55 +1,14 @@
 #!/usr/bin/python
 
 # import the necessary packages
+import geopandas as gpd
+import psycopg2
 import argparse
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Numeric
-from geoalchemy2 import Geometry
 import numpy as np
 from database import *
 from preprocessing import *
-
-Base = declarative_base()
-
-class poiGeoData_Marousi(Base):
-	__tablename__ = 'poiGeoData_Marousi'
 	
-	id = Column(Integer, primary_key=True)
-	geom = Column(Geometry('POLYGON'))
-	userid = Column(Integer)
-	class_code = Column(String(10))
-	theme = Column(String(254))
-	class_name = Column(String(254))
-	subclass_n = Column(String(254))
-	name_l = Column(String(254))
-	name_u = Column(String(254))
-	x = Column(Numeric)
-	y = Column(Numeric)
-	
-class edges(Base):
-	__tablename__ = 'edges'
-	
-	id = Column(Integer, primary_key=True)
-	geom = Column(Geometry('POLYGON'))
-	access = Column(String(80))
-	bridge = Column(String(80))
-	from2 = Column(String(80))
-	highway = Column(String(80))
-	junction = Column(String(80))
-	key = Column(String(80))
-	lanes = Column(String(80))
-	length = Column(String(80))
-	maxspeed = Column(String(80))
-	name = Column(String(80))
-	oneway = Column(String(80))
-	osmid = Column(String(165))
-	ref = Column(String(80))
-	service = Column(String(80))
-	to = Column(String(80))
-	tunnel = Column(String(80))
-	width = Column(String(80))
-	
-def get_poi_id_to_closest_street_id_dict(session):
+def get_poi_id_to_closest_street_id_dict(conn, args):
 
 	"""
 	*** This function maps each poi to its closest road id.
@@ -59,41 +18,28 @@ def get_poi_id_to_closest_street_id_dict(session):
 	***			  road id as its value.
 	"""
 	
-	from geoalchemy2 import functions
-	from sqlalchemy import func
-	
-	# get the pois
-	query = session.query(poiGeoData_Marousi)
-	poi_ids = []
-	for poi in query:
-		poi_ids.append(poi.id)
+	sql = "select {0}.id as poi_id, {0}.class_code, {0}.geom from {0}".format(args["pois_tbl_name"])
+	df = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
 	
 	# construct a dictionary from their ids
 	# also get its class_code
-	poi_id_to_edge_id_dict = dict.fromkeys(poi_ids)
-	for poi in query:
-		poi_id_to_edge_id_dict[poi.id] = [int(poi.class_code), 0]
-	
-	# get the roads
-	query2 = session.query(edges)
-	
-	# for each poi
-	for poi in query:
-		min_distance = 10000000000
-		# for each edge
-		for edge in query2:
-			# compute its distance with respect to the poi
-			distance = session.query(func.ST_Distance(func.ST_Transform(poi.geom, 32634), edge.geom))
-			# if its distance is smaller than the current minimum
-			if distance[0][0] < min_distance:
-				# update the minimum distance and map the
-				# road's id to the poi's id
-				min_distance = distance[0][0]
-				poi_id_to_edge_id_dict[poi.id][1] = edge.id
-					
+	poi_id_to_edge_id_dict = dict.fromkeys(df['poi_id'])
+	for index, row in df.iterrows():
+		poi_id_to_edge_id_dict[row['poi_id']] = [int(row['class_code']), 0]
+		
+	for index, row in df.iterrows():
+		# for each poi find its distance to all the roads in the dataset
+		sql = "select {0}.id as poi_id, {1}.id as edge_id, {0}.geom, ST_Distance(ST_Transform({0}.geom, 32634), {1}.geom) as dist from {0}, {1} where {0}.id = {2}".format(args["pois_tbl_name"], args["roads_tbl_name"], row['poi_id'])
+		dist_df = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
+		
+		# get the index of the minimum one and map the corresponding edge id to the poi
+		distance_list = list(dist_df['dist'])
+		min_index = distance_list.index(min(distance_list))
+		poi_id_to_edge_id_dict[row['poi_id']][1] = dist_df['edge_id'][min_index]
+
 	return poi_id_to_edge_id_dict
-	
-def get_street_id_to_closest_pois_boolean_and_counts_per_label_dict(session, threshold):
+
+def get_street_id_to_closest_pois_boolean_and_counts_per_label_dict(conn, threshold, args):
 	
 	"""
 	*** This function maps the street ids to their closest pois' label
@@ -101,51 +47,40 @@ def get_street_id_to_closest_pois_boolean_and_counts_per_label_dict(session, thr
 	*** whether they are located within threshold distance of the street
 	"""
 	
-	from geoalchemy2 import functions
-	from sqlalchemy import func
-	
 	# get all the pois
-	query = session.query(poiGeoData_Marousi)
-	
-	poi_ids = []
-	for poi in  query:
-		poi_ids.append(poi.id)
+	sql = "select {0}.id as poi_id, {0}.class_code, {0}.geom from {0}".format(args["pois_tbl_name"])
+	df_pois = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
 	
 	# create a dictionary with the poi ids as its keys
-	id_dict = dict.fromkeys(poi_ids)
-	for poi in query:
-		id_dict[poi.id] = [int(poi.class_code), 0, 0]
+	id_dict = dict.fromkeys(df_pois['poi_id'])
+	for index, row in df_pois.iterrows():
+		id_dict[row['poi_id']] = [int(row['class_code']), 0, 0]
 	
 	# get the class codes set and encode the class codes to labels
 	class_codes_set = get_class_codes_set()
-	id_to_encoded_labels_dict, encoded_labels_set = get_encoded_labels(class_codes_set, id_dict)
+	id_to_encoded_labels_dict, encoded_labels_set = get_poi_id_to_encoded_labels_dict(class_codes_set, id_dict)
 	num_of_labels = len(encoded_labels_set)
 	
-	# get the streets
-	query2 = session.query(edges)
-	
 	# create a dictionary with the street ids as its keys
-	street_ids = []
-	for edge in query2:
-		street_ids.append(edge.id)
-	street_id_to_closest_pois_boolean_and_counts_per_label_dict = dict.fromkeys(street_ids)
+	sql = "select {0}.id as edge_id, geom from {0}".format(args["roads_tbl_name"])
+	df_edges = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
+	street_id_to_closest_pois_boolean_and_counts_per_label_dict = dict.fromkeys(df_edges['edge_id'])
 	
 	# prepare the street id dictionary to be able to store the
 	# boolean and count duplet for each of the class labels
-	for edge in query2:
-		street_id_to_closest_pois_boolean_and_counts_per_label_dict[edge.id] = [[0,0] for _ in range(0, num_of_labels)]
+	for edge_id in street_id_to_closest_pois_boolean_and_counts_per_label_dict:
+		street_id_to_closest_pois_boolean_and_counts_per_label_dict[edge_id] = [[0,0] for _ in range(0, num_of_labels)]
+
+	for index, row in df_edges.iterrows():
+		# for each poi find its distance to all the roads in the dataset
+		sql = "select {0}.id as poi_id, {1}.id as edge_id, {0}.geom, ST_Distance(ST_Transform({0}.geom, 32634), {1}.geom) as dist from {0}, {1} where {1}.id = {2}".format(args["pois_tbl_name"], args["roads_tbl_name"], row['edge_id'])
+		dist_df = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
+		
+		for index, row in dist_df.iterrows():
+			if row['dist'] < threshold:
+				street_id_to_closest_pois_boolean_and_counts_per_label_dict[row['edge_id']][id_to_encoded_labels_dict[row['poi_id']][0][0]][0] = 1
+				street_id_to_closest_pois_boolean_and_counts_per_label_dict[row['edge_id']][id_to_encoded_labels_dict[row['poi_id']][0][0]][1] += 1
 	
-	# for each edge
-	for edge in query2:
-		# for each poi
-		for poi in query:
-			# within threshold distance
-			distance = session.query(func.ST_Distance(func.ST_Transform(poi.geom, 32634), edge.geom))
-			if distance[0][0] < threshold:
-				# update the boolean and count values
-				street_id_to_closest_pois_boolean_and_counts_per_label_dict[edge.id][encoded_labels_id_dict[poi.id][0][0]][0] = 1
-				street_id_to_closest_pois_boolean_and_counts_per_label_dict[edge.id][encoded_labels_id_dict[poi.id][0][0]][1] += 1
-				
 	return street_id_to_closest_pois_boolean_and_counts_per_label_dict
 	
 def update_poi_id_dictionary(poi_id_to_street_id_dict, street_id_to_closest_pois_boolean_and_counts_per_label_dict):
@@ -169,17 +104,19 @@ def update_poi_id_dictionary(poi_id_to_street_id_dict, street_id_to_closest_pois
 	return poi_id_to_closest_pois_boolean_count_dict
 	
 	
-def get_closest_pois_boolean_and_counts_per_label_streets(session, threshold = 0):
+def get_closest_pois_boolean_and_counts_per_label_streets(conn, args, threshold = 0):
 	
 	# get the dictionary mapping each poi id to that of its closest road
-	poi_id_to_closest_street_id_dict = get_poi_id_to_closest_street_id_dict(session)
+	poi_id_to_closest_street_id_dict = get_poi_id_to_closest_street_id_dict(conn, args)
+	#print(poi_id_to_closest_street_id_dict)
 	
 	# for every street id get the label boolean and counts values of the
 	# pois located within threshold distance from it
 	# (this will resemble the get_poi_id_to_boolean_and_counts_per_class_dict
 	#  function but with road ids as the keys of the dictionary)
-	threshold = 0
-	street_id_to_label_boolean_counts_dict = get_street_id_to_closest_pois_boolean_and_counts_per_label_dict(session, threshold)
+	threshold = 1000.0
+	street_id_to_label_boolean_counts_dict = get_street_id_to_closest_pois_boolean_and_counts_per_label_dict(conn, threshold, args)
+	return
 	
 	# construct a dictionary similar to the one returned by get_poi_id_to_boolean_and_counts_per_class_dict
 	# which will map a poi's id to the label boolean and count values of the poi's situated within threshold 
@@ -233,26 +170,26 @@ def get_poi_id_to_encoded_labels_dict(labels_set, id_dict):
 	
 	return id_dict, le.transform(labels_set)
 	
-def get_poi_id_to_class_code_coordinates_dict(session):
+def get_poi_id_to_class_code_coordinates_dict(conn, args):
 	
 	"""
 	*** This function returns a dictionary with poi ids as its keys and a 
 	*** list in the form of [< poi's class code >, < x coordinate > < y coordinate >]
 	*** as its values.
 	"""
-		
-	query = session.query(poiGeoData_Marousi)
-	poi_ids = []
-	for poi in  query:
-		poi_ids.append(poi.id)
-		
-	poi_id_to_class_code_coordinates_dict = dict.fromkeys(poi_ids)
-	for poi in query:
-		poi_id_to_class_code_coordinates_dict[poi.id] = [int(poi.class_code), float(poi.x), float(poi.y)]
+	
+	# get the pois
+	sql = "select {0}.id, {0}.class_code, {0}.x, {0}.y, {0}.geom from {0}".format(args["pois_tbl_name"])
+	df = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
+
+	poi_id_to_class_code_coordinates_dict = dict.fromkeys(df['id'])
+	
+	for index, row in df.iterrows():
+		poi_id_to_class_code_coordinates_dict[row['id']] = [int(row['class_code']), float(row['x']), float(row['y'])]
 	
 	return poi_id_to_class_code_coordinates_dict
 	
-def get_poi_id_to_boolean_and_counts_per_class_dict(session, num_of_labels, id_to_encoded_labels_dict, threshold):
+def get_poi_id_to_boolean_and_counts_per_class_dict(conn, num_of_labels, poi_id_to_encoded_labels_dict, threshold, args):
 	
 	"""
 	*** This function is responsible for mapping the pois to a list of two-element lists.
@@ -278,34 +215,32 @@ def get_poi_id_to_boolean_and_counts_per_class_dict(session, num_of_labels, id_t
 	dist = DistanceMetric.get_metric('euclidean')
 	
 	# get the poi ids and construct a dictionary with their ids as its keys
-	query = session.query(poiGeoData_Marousi)
-	poi_ids = []
-	for poi in  query:
-		poi_ids.append(poi.id)
-		
-	poi_id_to_label_boolean_counts_dict = dict.fromkeys(poi_ids)
+	
+	sql = "select {0}.id as poi_id, geom, x, y from {0}".format(args["pois_tbl_name"])
+	df = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col = 'geom')
+	
+	poi_id_to_label_boolean_counts_dict = dict.fromkeys(df['poi_id'])
 	
 	# add dummy values to the dictionary in order to initialize it
 	# in a form that resembles its desired final form
-	for poi in query:
-		poi_id_to_label_boolean_counts_dict[poi.id] = [[0,0] for _ in range(0, num_of_labels)]
+	for poi_id in poi_id_to_label_boolean_counts_dict:
+		poi_id_to_label_boolean_counts_dict[poi_id] = [[0,0] for _ in range(0, num_of_labels)]
 	
-	# for every two different pois
-	for id in poi_id_to_encoded_labels_dict:
-		for id2 in poi_id_to_encoded_labels_dict:
-			if id != id2:
+	for index1, row1 in df.iterrows():
+		for index2, row2 in df.iterrows():
+			if row1['poi_id'] != row2['poi_id']:
 				# get their coordinates
-				point1 = (poi_id_to_encoded_labels_dict[id][1], poi_id_to_encoded_labels_dict[id][2])
-				point2 = (poi_id_to_encoded_labels_dict[id2][1], poi_id_to_encoded_labels_dict[id2][2])
+				point1 = (row1['x'], row1['y'])
+				point2 = (row2['x'], row2['y'])
 				# if the two points are within treshold distance, 
 				# update the dictionary accordingly
 				if distance.euclidean(point1, point2) < threshold:
-					poi_id_to_label_boolean_counts_dict[id][poi_id_to_encoded_labels_dict[id2][0][0]][0] = 1
-					poi_id_to_label_boolean_counts_dict[id][poi_id_to_encoded_labels_dict[id2][0][0]][1] += 1
+					poi_id_to_label_boolean_counts_dict[row1['poi_id']][poi_id_to_encoded_labels_dict[row2['poi_id']][0][0]][0] = 1
+					poi_id_to_label_boolean_counts_dict[row1['poi_id']][poi_id_to_encoded_labels_dict[row2['poi_id']][0][0]][1] += 1
 	
 	return poi_id_to_label_boolean_counts_dict
 	
-def get_closest_pois_boolean_and_counts_per_label(session, threshold = 0):
+def get_closest_pois_boolean_and_counts_per_label(conn, args, threshold = 0):
 	
 	"""
 	*** This function returns a dictionary with the poi ids as its keys
@@ -320,7 +255,7 @@ def get_closest_pois_boolean_and_counts_per_label(session, threshold = 0):
 	
 	# we build a dictionary containing the poi ids as keys
 	# and we map to it its x, y coordinates
-	poi_id_to_class_code_coordinates_dict = get_poi_id_to_class_code_coordinates_dict(session)
+	poi_id_to_class_code_coordinates_dict = get_poi_id_to_class_code_coordinates_dict(conn, args)
 	
 	# we read the different labels
 	class_codes_set = get_class_codes_set()
@@ -328,29 +263,25 @@ def get_closest_pois_boolean_and_counts_per_label(session, threshold = 0):
 	# we encode them so we can have a more compact representation of them
 	poi_id_to_encoded_labels_dict, encoded_labels_set = get_poi_id_to_encoded_labels_dict(class_codes_set, poi_id_to_class_code_coordinates_dict)
 	
-	return get_poi_id_to_boolean_and_counts_per_class_dict(session, len(encoded_labels_set), poi_id_to_encoded_labels_dict, threshold)
+	return get_poi_id_to_boolean_and_counts_per_class_dict(conn, len(encoded_labels_set), poi_id_to_encoded_labels_dict, threshold, args)
 
 def main():
 	# construct the argument parse and parse the arguments
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-db_name", "--db_name", required=True,
-		help="database name")
-	ap.add_argument("-usr_name", "--usr_name", required=True,
-		help="name of the user")
-	ap.add_argument("-usr_pswd", "--usr_pswd", required=True,
-		help="password of the user")
+	ap.add_argument("-pois_tbl_name", "--pois_tbl_name", required=True,
+		help="name of table containing pois information")
+	ap.add_argument("-roads_tbl_name", "--roads_tbl_name", required=True,
+		help="name of table containing roads information")
 	args = vars(ap.parse_args())
 	
 	# call the appropriate function to connect to the database
-	engine = connect_to_db(args["db_name"], args["usr_name"], args["usr_pswd"])
+	conn = connect_to_db()
 	
-	# create a session to start querying the database
-	session = create_session(engine)
+	threshold = 1000.0
+	#closest_pois_boolean_and_counts_per_label = get_closest_pois_boolean_and_counts_per_label(conn, args, threshold)
+	#print(closest_pois_boolean_and_counts_per_label)
 	
-	#threshold = 1000.0
-	#closest_pois_boolean_and_counts_per_label = get_closest_pois_boolean_and_counts_per_label(session, threshold)
-	
-	#closest_pois_boolean_and_counts_per_label_streets = get_closest_pois_boolean_and_counts_per_label_streets(session)#, threshold)
+	closest_pois_boolean_and_counts_per_label_streets = get_closest_pois_boolean_and_counts_per_label_streets(conn, args, threshold)
 	
 if __name__ == "__main__":
    main()
