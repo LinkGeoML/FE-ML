d23f93684e14aeb10d068761d989e97e73337c37
MithrandirGr <nikos.kostagiolas@gmail.com>
2018-12-05 15:08:19 +0200

    Added 5-fold cross-validation experiments

--- geospatial-feature-extraction/simple_classification.py
+++ geospatial-feature-extraction/simple_classification.py
@@ -12,8 +12,11 @@ from textual_feature_extraction import *
 from feml import *
 import nltk
 import itertools
+import random
+
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -57,7 +60,68 @@ def plot_confusion_matrix(cm, classes,
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.tight_layout()
+    
+def default_parameters_single_fold(X_train, X_test, y_train, y_test):
+    # preprocess it
+	X_train, X_test = standardize_data(X_train, X_test)
+	
+	# declare classifiers
+	names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree", "Random Forest", "Naive Bayes"]
 
+	classifiers = [
+		KNeighborsClassifier(3),
+		SVC(kernel="linear", C=0.025),
+		SVC(gamma=2, C=1),
+		DecisionTreeClassifier(max_depth=5),
+		RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+		GaussianNB()]
+	
+	# iterate over classifiers and produce results
+	for name, clf in zip(names, classifiers):
+		clf.fit(X_train, y_train)
+		score = clf.score(X_test, y_test)
+		print("Accuracy Score of {0} classifier: {1}".format(name, score))
+		
+def default_parameters_5_fold(X_train, X_test, y_train, y_test):
+	
+	X = list(np.concatenate((X_train, X_test), axis=0))
+	y = list(np.concatenate((y_train, y_test), axis=0))
+	
+	c = list(zip(X, y))
+
+	random.shuffle(c)
+
+	X, y = zip(*c)
+	
+	X = np.asarray(X)
+	y = np.asarray(y)
+	
+	kf = KFold(n_splits=5)
+	count = 1
+	for train_index, test_index in kf.split(X):
+		X_train, X_test = X[train_index], X[test_index]
+		y_train, y_test = y[train_index], y[test_index]
+		
+		print("Displaying results for fold {0}".format(count))
+		
+		names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree", "Random Forest", "Naive Bayes"]
+
+		classifiers = [
+			KNeighborsClassifier(3),
+			SVC(kernel="linear", C=0.025),
+			SVC(gamma=2, C=1),
+			DecisionTreeClassifier(max_depth=5),
+			RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+			GaussianNB()]
+		
+		# iterate over classifiers and produce results
+		for name, clf in zip(names, classifiers):
+			clf.fit(X_train, y_train)
+			score = clf.score(X_test, y_test)
+			print("Accuracy Score of {0} classifier: {1}".format(name, score))
+			
+		count += 1
+		
 def main():
 	# construct the argument parse and parse the arguments
 	ap = argparse.ArgumentParser()
@@ -76,51 +140,27 @@ def main():
 	# call the appropriate function to connect to the database
 	conn = connect_to_db()
 	
-	poi_ids_train, poi_ids_test = get_train_test_poi_ids(conn, args)
-	X_train, y_train, X_test, y_test = get_train_test_sets(conn, args, poi_ids_train, poi_ids_test)
+	# get the data
 	
-	numpy.savetxt("train.csv", X_train, delimiter=",")
-	numpy.savetxt("test.csv", X_train, delimiter=",")
-	return
+	#poi_ids_train, poi_ids_test = get_train_test_poi_ids(conn, args)
+	#X_train, y_train, X_test, y_test = get_train_test_sets(conn, args, poi_ids_train, poi_ids_test)
 	
-	X_train, X_test = standardize_data(X_train, X_test)
+	# save it in csv files
 	
-	names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree", "Random Forest", "Naive Bayes"]
-
-	classifiers = [
-		KNeighborsClassifier(3),
-		SVC(kernel="linear", C=0.025),
-		SVC(gamma=2, C=1),
-		DecisionTreeClassifier(max_depth=5),
-		RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-		GaussianNB()]
-		
-	 # iterate over classifiers
-	for name, clf in zip(names, classifiers):
-		clf.fit(X_train, y_train)
-		score = clf.score(X_test, y_test)
-		
-		print("Classification Score of {0} classifier: {1}".format(name, score))
-		
-		print("Printing relevant confusion matrices...")
-		y_pred = clf.predict(y_test)
-		
-		# Compute confusion matrix
-		cnf_matrix = confusion_matrix(y_test, y_pred)
-		np.set_printoptions(precision=2)
-
-		# Plot non-normalized confusion matrix
-		plt.figure()
-		plot_confusion_matrix(cnf_matrix, classes=class_names,
-							  title='Confusion matrix, without normalization')
-
-		# Plot normalized confusion matrix
-		plt.figure()
-		plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
-							  title='Normalized confusion matrix')
-
-		plt.show()
-
+	#np.savetxt("train.csv", X_train, delimiter=",")
+	#np.savetxt("test.csv", X_test, delimiter=",")
+	#np.savetxt("labels_train.csv", y_train, delimiter=",")
+	#np.savetxt("labels_test.csv", y_test, delimiter=",")
+	
+	# load it from csv files
+	X_train = np.genfromtxt('train.csv', delimiter=',')
+	X_test = np.genfromtxt('test.csv', delimiter=',')
+	y_train = np.genfromtxt('labels_train.csv', delimiter=',')
+	y_test = np.genfromtxt('labels_test.csv', delimiter=',')
+	
+	#default_parameters_single_fold(X_train, X_test, y_train, y_test)
+	
+	default_parameters_5_fold(X_train, X_test, y_train, y_test)
 	
 if __name__ == "__main__":
    main()
