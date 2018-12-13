c1e5ec6aa12e4dc4514fce31ab32cb331af35c7c
Vassilis Kaffes <vkaffes@imis.athena-innovation.gr>
2018-12-13 18:48:27 +0200

    updated hyperparameters for DecisionTreeClassifier to run successfully under 16gb RAM

--- femlAlgorithms.py
+++ femlAlgorithms.py
@@ -445,12 +445,12 @@ class calcCustomFEML(baseMetrics):
             LinearSVC(random_state=0, C=1.0),
             # GaussianProcessClassifier(1.0 * RBF(1.0), n_jobs=3, warm_start=True),
             DecisionTreeClassifier(random_state=0, max_depth=50, max_features='auto'),
-            RandomForestClassifier(n_estimators=300, random_state=0, n_jobs=int(njobs), max_depth=60),
+            RandomForestClassifier(n_estimators=300, random_state=0, n_jobs=int(njobs), max_depth=50),
             MLPClassifier(alpha=1, random_state=0),
             AdaBoostClassifier(DecisionTreeClassifier(max_depth=50), n_estimators=300, random_state=0),
             GaussianNB(),
             QuadraticDiscriminantAnalysis(), LinearDiscriminantAnalysis(),
-            ExtraTreesClassifier(n_estimators=300, random_state=0, n_jobs=int(njobs), max_depth=60),
+            ExtraTreesClassifier(n_estimators=150, random_state=0, n_jobs=int(njobs), max_depth=50),
             XGBClassifier(n_estimators=3000, seed=0, nthread=int(njobs)),
         ]
         self.scores = [[] for _ in xrange(len(self.classifiers))]
